c3310fddaea52004c2531060aff4b75a62bf696b
Vhaily <scyllacharybdis33@gmail.com>
2022-03-03 20:36:06 +0100

    initial commit

---
+++ src/main/java/com/example/ShareYourNotes/ShareYourNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ShareYourNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareYourNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareYourNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShareYourNotes/ShareYourNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShareYourNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareYourNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
